1d8fdba33cbb34eff5061485c4988d7658c62333
MiriamVasco <84749175+MiriamVasco@users.noreply.github.com>
2021-05-24 22:27:30 -0500

    Add files via upload

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
-const MUJER = document.querySelector('.MUJER')
+const hamburguer = document.querySelector('.hamburguer')
 const menu = document.querySelector('.menu-navegacion')
 
 
-MUJER.addEventListener('click', ()=>{
+hamburguer.addEventListener('click', ()=>{
     menu.classList.toggle("spread")
 })
 
 window.addEventListener('click', e =>{
     if(menu.classList.contains('spread') 
-        && e.target != menu && e.target != MUJER){
+        && e.target != menu && e.target != hamburguer){
         console.log('cerrar')
         menu.classList.toggle("spread")
     }
